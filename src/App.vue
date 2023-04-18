f4975cb056cbfaff345540185ee195e09902ef05
Rat-Magician <74881352+Rat-Magician@users.noreply.github.com>
2023-04-18 08:14:10 +0300

    filter buttons (#3)
    * filter buttons
    
    * Add to storybook
    
    * fix fonts in storybook
    * minor component refactoring
    * move images to local env(experimental)
    
    * 0.1.5
    
    * Remove images
    
    ---------
    
    Co-authored-by: martiiian <jkramorevx@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <NewBlock />
+  <div />
 </template>
 
 <script setup lang="ts">
-import NewBlock from "./components/NewBlock/NewBlock.vue";
 </script>
---
+++ src/components/FilterButton/FilterButton.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="filter">
+    <button class="filter__button">
+      <img 
+        class="filter__calendar" 
+        src="./img/calendar.svg"
+        alt="calendar icon" 
+      >
+    </button>
+
+    <button class="filter__button">
+      <img 
+        class="filter__bookmark" 
+        src="./img/bookmark.svg"
+        alt="bookmark icon" 
+      >
+    </button>
+
+    <div class="filter__date-filter-state">
+      Показаны новости: <span class="filter__date-filter-dates">с {{ dateFrom }} - {{ dateTo }}</span>
+    </div>
+  </div>  
+</template>
+
+<script setup lang="ts">
+defineProps({
+  dateFrom: {
+    type: String,
+    required: true
+  },
+  dateTo: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss">
+  .filter {
+    display: flex;
+    align-items: center;
+    &__button {
+      background: #fbfbfb;
+      border-radius: 5px;
+      filter: drop-shadow(0px 2px 1px rgba(0, 0, 0, 0.13));
+      border: none;
+      margin-left: 23px;
+      cursor: pointer;
+
+      @media only screen and (max-width: 600px) {
+        width: 32px;
+        height: 32px;
+      }
+
+      @media only screen and (min-width: 1200px)  {
+        width: 54px;
+        height: 54px;
+
+        &:hover {
+          transition: 0.5s;
+          transform: scale(1.2);
+        }
+      }
+    }
+
+    &__calendar {
+      @media only screen and (max-width: 600px) {
+        width: 86%;
+      }
+
+      @media only screen and (min-width: 1200px)  {
+        width: 80%;
+      }
+    }
+
+    &__bookmark {
+      @media only screen and (max-width: 600px) {
+        width: 52%;
+      }
+
+      @media only screen and (min-width: 1200px)  {
+        width: 48%;
+      }
+    }
+
+    &__date-filter-state {
+      margin-left: 23px;
+      @media only screen and (max-width: 600px) {
+        display: none;
+      }
+      
+      @media only screen and (min-width: 1200px)  {
+        font-size: 0.88rem;
+      }
+    }
+
+    &__date-filter-dates {
+      font-weight: 600;
+    }
+  }
+</style>

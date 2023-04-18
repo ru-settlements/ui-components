0aecfc732bbacac764ee4801431268714774df63
martiiian <jkramorevx@gmail.com>
2023-04-18 08:41:37 +0300

    Fix export

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,3 @@
-import UIButton from '../src/components/UIButton/UIButton.vue'
-
-export { UIButton }
+export { default as UIButton } from '../src/components/UIButton/UIButton.vue'
+export { default as FilterButton } from '../src/components/FilterButton/FilterButton.vue'
+export { default as NewBlock } from '../src/components/NewBlock/NewBlock.vue'

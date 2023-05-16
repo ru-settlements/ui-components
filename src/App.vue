433dc55118a8b9076bf3da4ccc8d553012d46ad5
Rat-Magician <alexprototypemercer121@gmail.com>
2023-05-16 22:10:51 +0300

    search 1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-
+    <SearchBar />
 </template>
 
 <script setup lang="ts">
-import { SearchBar } from "@/components";
+  import { SearchBar } from "@/components";
 </script>
--- src/components/SearchBar/SearchBar.vue
+++ src/components/SearchBar/SearchBar.vue
@@ -1,39 +1,35 @@
 <template>
   <div class="search">
-    <form>
+    <form class="search__form">
       <input type="text" placeholder="Введите для поиска...">
       <button type="submit"></button>
     </form>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+
+</script>
 
 <style lang="scss">
-  body{
-    margin: 0;
-  }
-
-  div {
-    padding: 30px 0
-  }
-
-  form {
+  .search__form {
     position: relative;
-    width: 300px;
+    width: 451px;
     margin: 0 auto;
   }
 
   .search {background: #A3D0C3;}
   .search input {
     width: 100%;
-    height: 42px;
-    padding-left: 10px;
-    border: 2px solid #7BA7AB;
-    border-radius: 5px;
+    height: 50px;
+    padding-left: 21px;
+    border-radius: 11px;
     outline: none;
-    background: #F9F0DA;
     color: #9E9C9C;
+    border: none;
+    border-bottom: 1px solid $gray-700;
+    font-size: 15px;
+    
   }
 
   .search button {
@@ -47,7 +43,7 @@
     border-radius: 0 5px 5px 0;
     cursor: pointer;
   }
-  
+
   .search button:before {
     content: "\f002";
     font-family: FontAwesome;

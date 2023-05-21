5c6bfc40903d1895f5e39d6ac21e2123eef9b40c
Eugene <jkramorevx@gmail.com>
2023-05-22 00:15:43 +0300

    Update storybook
    * Upgrade storybook
    
    * Init AppSocial

---
+++ src/components/AppSocial/AppSocial.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-social">
+    <!-- here something-->
+    social
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AppSocial'
+}
+</script>
+
+<style lang="scss">
+
+</style>

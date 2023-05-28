69d132a09aff7a52837521a0677a6d02f705ba53
Rat-Magician <alexprototypemercer121@gmail.com>
2023-05-28 15:23:09 +0300

    add target _blank

--- src/components/AppSocial/AppSocial.vue
+++ src/components/AppSocial/AppSocial.vue
@@ -1,33 +1,50 @@
 <template>
   <div class="social">
     <div class="social__wrapper">
-      <a class="social__link">
+      <a 
+        class="social__link"
+        target="_blank"
+        href="#"
+        >
         <img 
-          class="social__icons"
+          class="social__icon"
           src="./img/whatsapp.svg"
           alt="whatsapp"
         >
       </a>
 
-      <a class="social__link">
+      <a 
+        class="social__link"
+        target="_blank"
+        href="#"
+        >
         <img 
-          class="social__icons"
+          class="social__icon"
           src="./img/vk.svg"
           alt="vkontakte"
+          target="_blank"
         >
       </a>
 
-      <a class="social__link">
+      <a 
+        class="social__link"
+        target="_blank"
+        href="#"
+        >
         <img 
-          class="social__icons"
+          class="social__icon"
           src="./img/ok.svg"
           alt="odnoklassniki"
         >
       </a>
 
-      <a class="social__link">
+      <a 
+        class="social__link"
+        target="_blank"
+        href="#"
+        >
         <img 
-          class="social__icons"
+          class="social__icon"
           src="./img/tg.svg"
           alt="telegram"
         >

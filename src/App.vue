5def9c7111f8dc9f493c77cd655e7eb98518c112
Rat-Magician <alexprototypemercer121@gmail.com>
2023-06-08 17:26:57 +0300

    add two buttons

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,7 @@
 <template>
-  <NewBlock v-bind="args" />
+  <LoginButton />
 </template>
 
 <script setup lang="ts">
-import NewBlock from '@/components/NewBlock/NewBlock.vue'
-
-const args = {
-  title: 'Бабка поймала большого сома',
-  description: `
-  И нет сомнений, что ключевые особенности
-            структуры проекта формируют глобальную
-            экономическую сеть и при этом — рассмотрены
-
-            исключительно в разрезе маркетинговых и
-            финансовых предпосылок.
-            <br>
-            <br>
-            И нет сомнений, что ключевые особенности
-            структуры проекта формируют глобальную
-            экономическую сеть и при этом — рассмотрены
-            исключительно в разрезе маркетинговых и
-            финансовых предпосылок.`,
-  date: '20.03.2023',
-  imgLink: '/src/assets/images/babka.png',
-  tags: [{
-    color: 'blue',
-    name: 'медведица'
-  },
-  {
-    color: 'green',
-    name: 'рыбалка'
-  }]
-}
+import LoginButton from '@/components/LoginButton/LoginButton.vue'
 </script>

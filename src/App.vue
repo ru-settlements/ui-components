8133d8aeb7c7d834fc636d59576fd3ddcd2fce20
Rat-Magician <alexprototypemercer121@gmail.com>
2023-06-12 16:37:31 +0300

    add branch

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <LoginButton icon-path="/src/components/LoginButton/img/telegram.svg">
-    Зарегистрироваться
-  </LoginButton>
 </template>
 
 <script setup lang="ts">
-import LoginButton from '@/components/UIButton/UIButton.vue'
 </script>
